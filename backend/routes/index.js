6483fc997ee255c0901cda12ef687dd8ca5fc8b7
Sauhardha20 <t2khsa00@students.oamk.fi>
2025-01-17 18:50:49 +0200

    changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,19 @@ import express from "express";
 import topScorersRoutes from "./topScorers.js";
 import topAssistsRoutes from "./topAssists.js";
 import fixturesRoutes from "./fixtures.js";
+import standingsRoutes from "./standings.js";
+import topYellowCardsRoutes from "./topYellowCards.js";
+import topRedCardsRoutes from "./topRedCards.js";
+import teamMatchesRoutes from './teamMatches.js';
 
 const router = express.Router();
 
 router.use("/topscorers", topScorersRoutes);
 router.use("/topassists", topAssistsRoutes);
 router.use("/fixtures", fixturesRoutes);
+router.use("/standings", standingsRoutes);
+router.use("/topyellowcards", topYellowCardsRoutes);
+router.use("/topredcards", topRedCardsRoutes);
+router.use('/team-matches', teamMatchesRoutes);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import fixturesRoutes from "./routes/fixtures.js";
-import standingsRoutes from "./routes/standings.js";
-import topScorersRoutes from "./routes/topScorers.js";
-import topAssistsRoutes from "./routes/topAssists.js";
-import topYellowCardsRoutes from "./routes/topYellowCards.js";
-import topRedCardsRoutes from "./routes/topRedCards.js";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
@@ -15,13 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "http://localhost:5173" }));
 
+// Middleware
+app.use(express.json());
+
 // Routes
-app.use("/api/fixtures", fixturesRoutes);
-app.use("/api/standings", standingsRoutes);
-app.use("/api/topscorers", topScorersRoutes);
-app.use("/api/topassists", topAssistsRoutes);
-app.use("/api/topyellowcards", topYellowCardsRoutes);
-app.use("/api/topredcards", topRedCardsRoutes);
+app.use("/api", routes);
 
 // Start server
 app.listen(PORT, () => {
